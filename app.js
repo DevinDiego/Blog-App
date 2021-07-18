87fe1cc3a5f57971a63d9ae4234902779cae5bbb
DevinDiego <tuffycatdevin@gmail.com>
2021-07-17 17:37:22 -0700

    add Procfile

--- app.js
+++ app.js
@@ -14,7 +14,11 @@ const dbURI =
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
-    app.listen(3000);
+    let port = process.env.PORT;
+    if (port == null || port == "") {
+      port = 3000;
+    }
+    app.listen(port);
   })
   .catch((err) => {
     console.log(err);
