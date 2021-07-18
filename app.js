0293b116a0365b124dc6641f9c57b613ef8b88f8
DevinDiego <tuffycatdevin@gmail.com>
2021-07-17 17:22:13 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const dbURI =
+  "mongodb+srv://admin-devin:CoreChewzz@myownblogcluster.2e7gf.mongodb.net/my-own-blog-db?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", {
+        title: "Home",
+        blogs: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+  });
+});
+
+app.get("/create", (req, res) => {
+  res.render("create", {
+    title: "Create",
+  });
+});
+
+app.post("/", (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/details/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", {
+        title: "Blog Details",
+        blog: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.delete("/details/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
